dc315b7cbcc099fe0a795ec611e04b7934d73510
ejin3003 <ejin3003@gmail.com>
2020-04-21 21:53:01 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+n95 = pd.read_excel(r"C:\Users\ejin3\OneDrive\Desktop\N95 Report.xlsx")
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', None)
+n95.columns = ['Mask', 'Date', 'Unit', 'Qty']
+n95['Mask'] = n95['Mask'].astype('category')
+n95['Date'] = n95['Date'].dt.date
+
+n95['Mask'].replace({
+    'FILTER PFR95 RESPIRATOR REG SIZE': 'Halyard Duckbill Reg',
+    'GERSON N95 MASK RESPIRATOR 1730': 'Gerson 1730',
+    'GERSON N95 MASK RESPIRATOR 82130': 'Gerson 82130',
+    'MASK FACE RESPIRATOR N95 SMALL BX/20EA': '3M 1860S Small',
+    'MASK RESPIRATOR PARTICULATE CONE N95 NIOSH CERTIFIED FLUID R': '3M 1860 Regular',
+    'MASK RESPIRATOR SM CS/6BX/35EA': 'Halyard Duckbill Small'
+}, inplace=True)
+
+# Total per each Group
+# mgh_units = n95.groupby(['Unit', 'Date', 'Mask'])['Qty'].sum().unstack(level=['Date', 'Mask'])
+# mgh_units = mgh_units.assign(total=mgh_units.sum(1))
+# mgh_units = mgh_units.stack(level='Date')
+# mgh_units = mgh_units.assign(total=mgh_units.sum(1))
+# mgh_units = mgh_units.stack(level='Mask')
+# print(mgh_units)
+# mgh_units.to_excel(r"C:\Users\ejin3\OneDrive\Desktop\test2.xlsx")
+#
+
+mgh_units = n95.groupby(['Unit', 'Date', 'Mask'])
+
+mgh_units.sum().to_excel(r"C:\Users\ejin3\OneDrive\Desktop\test2.xlsx")
+
+# multi_index_df = n95.set_index(keys=['Date', 'Unit'])
+# multi_index_df.to_excel(r"C:\Users\ejin3\OneDrive\Desktop\test.xlsx")
+
+
