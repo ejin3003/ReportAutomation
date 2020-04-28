d58f41314c225a1adb16e2b0e8f0db899dc65c61
Ejin <ejin3003@gmail.com>
2020-04-28 18:48:56 -0400

    2nd step to organizing code within definitions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Renames columns and mask names within the Customer Service N95 Report
-from rename_report_cells import *
-path = r"C:\Users\ejin3\OneDrive\Desktop\N95 Report 4.27.xlsx"
-dest = r"C:\Users\ejin3\OneDrive\Desktop\New N95 Report 4.27.xlsx"
-rename_n95_report(path, dest)
+from Report_Tools.rename_report_cells import *
+n95_report_path = r"C:\Users\ejin3\OneDrive\Desktop\N95 Report 4.27.xlsx"
+n95_report_dest = r"C:\Users\ejin3\OneDrive\Desktop\New N95 Report 4.27.xlsx"
+rename_n95_report(n95_report_path, n95_report_dest)
+
+# Appends units totals to n95_report_dest
