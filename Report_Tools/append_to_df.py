cb333464d52e26205ea1b1d545a29c01ca540130
Ejin <ejin3003@gmail.com>
2020-04-30 22:08:47 -0400

    Phase 1 of N95 Report Complete Daily Totals Added to the N95 Report

---
+++ Report_Tools/append_to_df.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from openpyxl import load_workbook
+from datetime import date
+from datetime import timedelta
+
+
+def add_daily_total(path, dest, ref):
+    n95_daily_totals = pd.read_excel(path)
+    df = pd.read_excel(ref)
+
+    n95_daily_totals["Date"] = n95_daily_totals["Date"].dt.date
+
+    total = df["Qty"].sum()
+    yesterday = date.today() - timedelta(days=1)
+
+    df = [{
+        "Date": yesterday,
+        "Total": total
+    }]
+
+    n95_daily_totals = n95_daily_totals.append(df, ignore_index=True)
+
+    n95_daily_totals.set_index(keys="Date", inplace=True)
+    n95_daily_totals.sort_index(ascending=False, inplace=True)
+    print(n95_daily_totals)
+
+    # Add Daily Total to the N95 Report
+    writer = pd.ExcelWriter(dest, engine="openpyxl")
+    book = load_workbook(dest)
+    writer.book = book
+    n95_daily_totals.to_excel(writer, sheet_name="Daily Totals")
+    sheet = book.get_sheet_by_name("Daily Totals")
+    sheet.column_dimensions["A"].width = 14
+    sheet.column_dimensions["B"].width = 14
+    writer.save()
+    writer.close()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Report_Tools.rename_report_cells import *
 from Report_Tools.unit_totals import *
 from Report_Tools.format_sheets import *
+from Report_Tools.append_to_df import *
 from datetime import date
 from datetime import timedelta
 
@@ -15,7 +16,9 @@ yesterday = date.today() - timedelta(days=1)
 n95_report_path = r"C:\Users\ejin3\OneDrive\Desktop\N95 Report.xlsx"
 unit_total_path = r"C:\Users\ejin3\OneDrive\Desktop\Raw CS Data N95.xlsx"
 n95_report_dest = r"C:\Users\ejin3\OneDrive\Desktop\CS N95 Report {}.xlsx".format(yesterday)
+daily_totals = r"C:\Users\ejin3\OneDrive\Desktop\N95 Daily Totals.xlsx"
 
 rename_n95_report(n95_report_path, n95_report_dest)
 unit_totals(unit_total_path, n95_report_dest)
 format_report(n95_report_dest)
+add_daily_total(daily_totals, n95_report_dest, unit_total_path)
