a7d2b7aefde5137b0bdc94d9d6963fbc5c8e1fa1
Ejin <ejin3003@gmail.com>
2020-10-22 09:34:23 -0400

    Building Reusable Tools for Manipulating Reports:
    1. Building a tool for renaming columns within a dataframe

---
+++ rep_tools/alter/alter_colm.py
@@ -0,0 +1,17 @@
+
+class AlterColumns:
+    """
+    Functions designed to alter columns names within a dataframe
+    """
+    def __init__(self, df, colm_lst):
+        self.df = df,
+        self.colm_lst = colm_lst
+
+    def alter_colm_names(self):
+        """
+        1. Checks if the list of column names matches the total number of columns within the dataframe
+        2. If list_colm_names = total_df_colm then rename the df colm according to the list
+
+        """
+        pass
+
