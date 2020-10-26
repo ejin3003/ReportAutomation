736970e49efa09f31444d7b5a23e7811229798cc
ejin3003 <ejin3003@gmail.com>
2020-10-26 18:41:31 -0400

    Isolated Unique Locations & Location ID's

--- dept/mats_serv/ms_app/read_cart_items.py
+++ dept/mats_serv/ms_app/read_cart_items.py
@@ -1,6 +1,20 @@
 import pandas as pd
-path = r"C:\Users\tyson\OneDrive\Desktop\MS\JB_CART_ITEMS_ALL_PHS_CARTS\JB_CART_ITEMS_MGH-26812916.csv"
+# path = r"C:\Users\tyson\OneDrive\Desktop\MS\JB_CART_ITEMS_ALL_PHS_CARTS\JB_CART_ITEMS_MGH-26812916.csv"
+path = r"C:\Users\ejin3\OneDrive\Desktop\Materials Service\MS Server Dump\JB_CART_ITEMS_ALL_PHS_CARTS\JB_CART_ITEMS_MGH-26812916.csv"
 # path.encode('utf-8').strip()
 df_1 = pd.read_csv(path, encoding='unicode_escape')
+loc_id_obj = df_1.groupby("Par Location ID")
+# loc_df = loc_id_obj["Location"]
 
-print(df_1.head())
+# DataFrame of Locations and Par Location ID
+unit_test = df_1.drop_duplicates(subset=["Location", "Par Location ID"])
+unit_test = unit_test[["Par Location ID", "Location"]].sort_values("Par Location ID")
+print(unit_test.count())
+
+# Sorts DataFrame by specified values
+# df_1.sort_values("Par Location ID", inplace=True)
+
+# Create a DataFrame of unique values
+# units = df_1.Location.unique()
+# unit_df = pd.DataFrame(units)
+
