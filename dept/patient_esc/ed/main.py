3715477b850cafd8d8888f9650baa939ae88d920
Ejin <ejin3003@gmail.com>
2020-11-27 13:51:40 -0500

    Building sample report for the ew radiology

--- dept/patient_esc/ed/main.py
+++ dept/patient_esc/ed/main.py
@@ -19,15 +19,10 @@ sector_list = ["MGH ED Radiology", "MGH RAD XRAY ER WH1"]
 df = df[df["Sector"].isin(sector_list)]
 
 # Converts designated string columns into number columns
-# df["Ack->Cmp"] = df["Ack->Cmp"].str.extract(r'(\d*\.?\d*)', expand=False).astype(int)
-# df["Ack->InP"] = df["Ack->InP"].str.extract(r'(\d*\.?\d*)', expand=False).astype(int)
-
-col_lst = ["Ack->Cmp", "Ack->InP"]
-
+col_lst = ["Ack->Cmp", "Ack->InP", "Asgn->Ack", "Asgn->Cmp", "InP->Cmp", "Pnd->Asgn", "Pnd->Cmp", "Total Delay Time"]
 for num in range(len(col_lst)):
     col = col_lst[num]
     df[col] = df[col].str.extract(r'(\d*\.?\d*)', expand=False).astype(int)
 
-# print("Destination Dept: " + str(df["Destination Department"].nunique()))
-# print("Pick-Up Dept: " + str(df["Pick-Up Department"].nunique()))
-# print(df.count())
+# Removes time from "Transport Date" column
+df["Transport Date"] = df["Transport Date"].dt.date
