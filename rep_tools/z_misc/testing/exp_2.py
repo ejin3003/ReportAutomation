10d5211765ecf9724f2a39bc80ba44df894f7368
ejin3003 <ejin3003@gmail.com>
2021-03-24 12:46:35 -0400

    Updating Pillow: Security Alert

--- rep_tools/z_misc/testing/exp_2.py
+++ rep_tools/z_misc/testing/exp_2.py
@@ -1,13 +1,30 @@
+import timeit
 
-dct = {
-    "One": 1,
-    "Two": 2,
-    "Three": 3
-}
-
-
-for _, k in enumerate(dct):
-    val = dct[k]
-    print(k, val)
+"""
+Sum the numbers from 0 to n-1 in different ways.
+"""
 
 
+def while_loop(n=100_000_000):
+    i = 0
+    s = 0
+    while i < n:
+        s += i
+        i += 1
+    return s
+
+
+def for_loop(n=100_000_000):
+    s = 0
+    for i in range(n):
+        s += i
+    return s
+
+
+def main():
+    print('While Loop:\t\t', timeit.timeit(while_loop, number=1))
+    print('For Loop:\t\t', timeit.timeit(for_loop, number=1))
+
+
+if __name__ == '__main__':
+    main()
