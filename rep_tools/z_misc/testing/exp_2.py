0f7a795b13c9a94466b51725976de52f28f52cf7
jt883 <ejin3003@gmail.com>
2021-03-30 14:52:01 -0400

    Testing join_dataframes function

--- rep_tools/z_misc/testing/exp_2.py
+++ rep_tools/z_misc/testing/exp_2.py
@@ -1,30 +1,9 @@
-import timeit
-
-"""
-Sum the numbers from 0 to n-1 in different ways.
-"""
+# print( 2 + 2 )
 
 
-def while_loop(n=100_000_000):
-    i = 0
-    s = 0
-    while i < n:
-        s += i
-        i += 1
-    return s
+def twoplustwo(x, y):
+    total = x + y
+    return total
 
-
-def for_loop(n=100_000_000):
-    s = 0
-    for i in range(n):
-        s += i
-    return s
-
-
-def main():
-    print('While Loop:\t\t', timeit.timeit(while_loop, number=1))
-    print('For Loop:\t\t', timeit.timeit(for_loop, number=1))
-
-
-if __name__ == '__main__':
-    main()
+total = twoplustwo(1000, 2000)
+print(total)
